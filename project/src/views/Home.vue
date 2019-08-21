4409e447381eda98a8c3fdc67b31fdc93171fc94
Raphi <raphael.antonietti@liip.ch>
2019-08-21 08:14:30 +0200

    Initial tailwind setup

---
+++ project/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <span class="text-center bg-black p-8 text-white">Does tailwind work?</span>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
